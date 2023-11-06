7b2f142e454826b85f7fdc98be86e528b9350005
chelGomes <68570496+chelGomes@users.noreply.github.com>
2023-11-05 22:43:54 -0300

    implementacao caso de teste

---
+++ src/test/java/CompraVisitorTeste.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CompraVisitorTeste {
+    @Test
+    void deveExibirProduto() {
+        Produto produto = new Produto("notebook", 2000);
+
+        CompraVisitor visitor = new CompraVisitor();
+        assertEquals("Produto{nomeProduto=notebook, preco=2000}", visitor.exibir(produto)); //corrigir
+    }
+
+    @Test
+    void deveExibirEntrega() {
+        Entrega entrega = new Entrega("Joao",15.0,"aparelho eletronico");
+
+        CompraVisitor visitor = new CompraVisitor();
+        assertEquals("Entrega{numeroPedido=110}", visitor.exibir(entrega)); //corrir
+        
+    }
+
+    @Test
+    void deveExibirTipoPagamento() {
+        TipoPagamento tipoPagamento = new TipoPagamento("cartao", 9, 2);
+
+        CompraVisitor visitor = new CompraVisitor();
+        assertEquals("TipoPagamento{formaPagamento=cartao, numeroParcelasMaximas='9', numeroParcelasMinimas=2}", visitor.exibir(tipoPagamento));
+    }
+}
